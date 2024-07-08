bb8a1a17d69501d414dbcb4cf6da1265ee8d7a0a
João Vaz <119084806+jg2alv@users.noreply.github.com>
2024-07-08 00:13:26 -0300

    inicio da leitura da entrada

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
+#include <string>
 
+#include "Jogador.h"
+#include "Sistema.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Sistema* sistema = new Sistema();
+
+    while (1)
+    {
+        std::string comando;
+        std::cin >> comando;
+        Comando comandoAnalisado = sistema->AnalisarComando(comando);
+
+        switch (comandoAnalisado)
+        {
+        case Comando::CJ: {
+            std::string apelido, nome;
+            std::cin >> apelido >> nome;
+            std::cout << apelido << std::endl;
+            break;
+        }
+        
+        case Comando::RJ:
+            break;
+
+        case Comando::LJ:
+            break;
+
+        case Comando::EP:
+            break;
+
+        case Comando::FS:
+            break;
+
+        case Comando::IN:
+            break;
+        }
+    }
+
+    free(sistema);
 
     return 0;
 }
-
