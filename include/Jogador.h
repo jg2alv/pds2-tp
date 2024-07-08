02212b003dafcc58a9dfbb4ba5400ae8de2f1844
João Vaz <119084806+jg2alv@users.noreply.github.com>
2024-07-08 00:12:36 -0300

    add header file pra jogador

---
+++ include/Jogador.h
@@ -0,0 +1,16 @@
+#ifndef JOGADOR_INCLUDED
+#define JOGADOR_INCLUDED
+
+#include <string>
+
+class Jogador
+{
+private:
+    std::string _nome;
+    std::string _apelido;
+
+public:
+    Jogador();
+};
+
+#endif
