695b7f86b1a1220ae0d87666dbd3f62b8520d25a
DAVI SAD <davioliveirasad@gmail.com>
2024-07-08 17:17:54 -0300

    Começo do header do Reversi, to no aguardo da classe pai

---
+++ include/Reversi.hpp
@@ -0,0 +1,32 @@
+#ifndef REVERSI_INCLUDED
+#define REVERSI_INCLUDED
+
+#include <iostream>
+#include <Jogador.hpp>
+
+class Reversi : public Jogos
+{
+    private:
+    Jogador _jogador1;
+    Jogador _jogador2;
+
+    public:
+
+    Reversi(){}
+    void setJogadores (Jogador jogador1, Jogador jogador2) {}
+    ~Reversi(){}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
