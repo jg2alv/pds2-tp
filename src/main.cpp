c4e13cb2db56f5453dbe89fddaacb8d685334dd1
João Vaz <119084806+jg2alv@users.noreply.github.com>
2024-07-10 19:17:24 -0300

    nova forma de sair do sistema: met. especializado

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,14 @@
 int main() {
     Sistema sistema;
 
-    while (true) {
+    while (!sistema.isSistemaFinalizado()) {
         std::string comando; std::cin >> comando;
         Comando comando_analisado = sistema.analisarComando(comando);
         try {
             std::string saida = sistema.executarComando(comando_analisado);
-
             bool saida_vazia = (saida.length() == 0);
-            bool sair = (saida == "FS");
 
-            if(sair) return 0;
-            else if(!saida_vazia) std::cout << saida << std::endl;
+            if(!saida_vazia) std::cout << saida << std::endl;
         }
         catch(const std::exception& e) {
             std::cerr << "ERRO: " << e.what() << std::endl;
