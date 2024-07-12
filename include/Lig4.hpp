cc962b8f57d43940ffde2b9900b3c991293a1a55
MEduardaNunes <mecosta961@gmail.com>
2024-07-11 21:36:32 -0300

    Adicionando funcoes do lig4

--- include/Lig4.hpp
+++ include/Lig4.hpp
@@ -7,14 +7,17 @@
 class Lig4 : public Jogo
 {
 private:
-    Jogador _jogador1;
-    Jogador _jogador2;
+    Jogador *_jogador1;
+    Jogador *_jogador2;
 
 public:
-    Lig4();
-    void setJogadores(Jogador jogador1, Jogador jogador2);
-    ~Lig4();
+    Lig4(int rows, int cols, Jogador *jogador1, Jogador *jogador2);
+
+    void setJogadores(Jogador *jogador1, Jogador *jogador2);
+    bool lerJogada(int player, int row, int col);
+    bool jogadaValida(int row, int col) const;
+    bool verificarVitoria(int player) const;
+    bool verificarEmpate() const;
 };
 
 #endif
-
