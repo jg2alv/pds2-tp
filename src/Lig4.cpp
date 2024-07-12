76a8e9ff32d2157921afa26decefd63847ae1f94
MEduardaNunes <mecosta961@gmail.com>
2024-07-11 21:38:07 -0300

    Adiconando funcoes do lig4

--- src/Lig4.cpp
+++ src/Lig4.cpp
@@ -1,9 +1,29 @@
+#include <iostream>
+#include "Jogo.hpp"
 #include "Lig4.hpp"
 
-void Lig4::setJogadores(Jogador jogador1, Jogador jogador2)
+Lig4::Lig4(int rows, int cols, Jogador *jogador1, Jogador *jogador2) : Jogo(rows, cols)
+{
+    this->_jogador1 = jogador1;
+    this->_jogador2 = jogador2;
+}
+
+void Lig4::setJogadores(Jogador *jogador1, Jogador *jogador2)
 {
 }
 
-Lig4::~Lig4()
+bool Lig4::lerJogada(int player, int row, int col)
+{
+}
+
+bool Lig4::jogadaValida(int row, int col) const
+{
+}
+
+bool Lig4::verificarVitoria(int player) const
+{
+}
+
+bool Lig4::verificarEmpate() const
 {
 }
