d3a2d2b64274897cc4a78b2625995dc36c804305
MEduardaNunes <mecosta961@gmail.com>
2024-07-21 10:38:24 -0300

    modificando metodo de Jogo e Lig4, e melhorando a estetica

--- include/Lig4.hpp
+++ include/Lig4.hpp
@@ -19,13 +19,15 @@ private:
 
 public:
     Lig4(int linhas, int colunas, Jogador &jogador1, Jogador &jogador2);
+    void partida();
     void imprimirTabuleiro() const;
-    Jogada lerJogada(int jogador) override;
+    Jogada lerJogada() override;
     void realizarJogada(Jogada &jogada);
     bool verificarVitoria() const override;
     bool verificarEmpate() const override;
     void reiniciarTabuleiro() override;
     void mudarTurno();
+
 };
 
 #endif
--- src/Lig4.cpp
+++ src/Lig4.cpp
@@ -4,100 +4,70 @@
 #include "Jogador.hpp"
 #include "Jogo.hpp"
 
-int Lig4::get_linha(int coluna) const
-{
-    if (get_char(0, coluna) != ' ')
-        return -1;
+Lig4::Lig4(int linhas, int colunas, Jogador &jogador1, Jogador &jogador2)
+    : Jogo(linhas, colunas, jogador1, jogador2) {
+    this->jogador_atual = 1;
+}
 
-    for (int i = (this->linhas - 1); i >= 0; i--)
-    {
-        if (get_char(i, coluna) == ' ')
-            return i;
+int Lig4::get_linha(int coluna) const {
+    if (get_char(0, coluna) != ' ') return -1;
+
+    for (int i = (this->linhas - 1); i >= 0; i--) {
+        if (get_char(i, coluna) == ' ') return i;
     }
 }
 
-bool Lig4::jogadaValida(Jogada &jogada) const
-{
+bool Lig4::jogadaValida(Jogada &jogada) const {
     int coluna = jogada.get_coluna();
-    int linha = this->get_linha(coluna);
+    int linha = jogada.get_linha();
 
-    if (coluna >= 1 && coluna <= this->colunas && linha != -1)
-    {
-        jogada.set_linha(linha);
-        return true;
+    if (coluna >= 1 && coluna <= this->colunas && linha != -1) return true;
+    else return false;
+}
+
+bool Lig4::linhaVazia(int linha) const {
+    for (int j = 0; j < this->colunas; j++) {
+        if (get_char(linha, j) != ' ') return false;
     }
-    else
-        return false;
-}
-
-bool Lig4::linhaVazia(int linha) const
-{
-    for (int j = 0; j < this->colunas; j++)
-        if (get_char(linha, j) != ' ')
-            return false;
 
     return true;
 }
 
-bool Lig4::colunaVazia(int coluna) const
-{
-    for (int i = 0; i < this->linhas; i++)
-        if (get_char(i, colunas) != ' ')
-            return false;
+bool Lig4::colunaVazia(int coluna) const {
+    for (int i = 0; i < this->linhas; i++) {
+        if (get_char(i, colunas) != ' ') return false;
+    }
 
     return true;
 }
 
-bool Lig4::tabuleiroCheio() const
-{
-    for (int i = 0; i < this->linhas; i++)
-        for (int j = 0; j < this->colunas; j++)
-            if (get_char(i, j) == ' ')
-                return false;
-
-    return true;
-}
-
-void Lig4::imprimirVitoria() const
-{
+void Lig4::imprimirVitoria() const {
     if (this->jogador_atual == 1) std::cout << jogador1.getApelido();
     else if (this->jogador_atual == 2) std::cout << jogador2.getApelido();
     std::cout << " venceu a partida!" << std::endl;
 }
 
-void Lig4::imprimirEmpate() const
-{
+void Lig4::imprimirEmpate() const {
     std::cout << jogador1.getApelido() << " e ";
     std::cout << jogador2.getApelido() << " empataram!" << std::endl;
 }
 
-Lig4::Lig4(int linhas, int colunas, Jogador &jogador1, Jogador &jogador2)
-    : Jogo(linhas, colunas, jogador1, jogador2)
-{
-    this->jogador_atual = 1;
-}
-
-void Lig4::imprimirTabuleiro() const
-{
-    for (int i = 0; i < this->linhas; i++)
-    {
-        for (int j = 0; j < this->colunas; j++)
-        {
+void Lig4::imprimirTabuleiro() const {
+    for (int i = 0; i < this->linhas; i++) {
+        for (int j = 0; j < this->colunas; j++) {
             std::cout << "|" << get_char(i, j) << "|";
         }
         std::cout << std::endl;
     }
 
-    for (int i = 1; i <= this->colunas; i++)
-    {
+    for (int i = 1; i <= this->colunas; i++) {
         std::cout << " " << i << " ";
     }
-    std::cout << std::endl
-              << std::endl;
+
+    std::cout << std::endl << std::endl;
 }
 
-Jogada Lig4::lerJogada(int jogador)
-{
+Jogada Lig4::lerJogada() {
     std::cout << "Turno do jogador ";
     if (this->jogador_atual == 1) std::cout << this->jogador1.getApelido();
     else if (this->jogador_atual == 2) std::cout << this->jogador2.getApelido();
@@ -106,29 +76,23 @@ Jogada Lig4::lerJogada(int jogador)
     int coluna;
     std::cin >> coluna;
     Jogada jogada(coluna);
+    jogada.set_linha(get_linha(coluna));
 
     return jogada;
 }
 
-void Lig4::realizarJogada(Jogada &jogada)
-{
-    if (jogadaValida(jogada) == true)
-    {
+void Lig4::realizarJogada(Jogada &jogada) {
+    if (jogadaValida(jogada)) {
         char simbolo;
         if (this->jogador_atual == 1) simbolo = this->simbolo_jogador1;
         else if (this->jogador_atual == 2) simbolo = this->simbolo_jogador2;
 
         set_char(jogada.get_linha(), jogada.get_coluna(), simbolo);
-    }
-    else
-    {
-        std::cout << "ERRO: jogada invalida" << std::endl;
-        lerJogada(this->jogador_atual);
-    }
+
+    } else std::cout << "ERRO: jogada invalida" << std::endl;
 }
 
-bool Lig4::verificarVitoria() const
-{
+bool Lig4::verificarVitoria() const {
     char simbolo;
     if (this->jogador_atual == 1) simbolo = simbolo_jogador1;
     if (this->jogador_atual == 2) simbolo = simbolo_jogador2;
@@ -191,28 +155,34 @@ bool Lig4::verificarVitoria() const
     return false;
 }
 
-bool Lig4::verificarEmpate() const
-{
-    if (tabuleiroCheio() == true)
-    {
-        imprimirEmpate();
-        return true;
+bool Lig4::tabuleiroCheio() const {
+    for (int i = 0; i < this->linhas; i++) {
+        for (int j = 0; j < this->colunas; j++) {
+            if (get_char(i, j) == ' ') return false;
+        }
     }
-    else
-        return false;
+
+    return true;
 }
 
-void Lig4::reiniciarTabuleiro()
-{
-    for (int i = 0; i < this->linhas; i++)
-        for (int j = 0; j < this->colunas; j++)
+bool Lig4::verificarEmpate() const {
+    if (tabuleiroCheio() == true) return true;
+    else return false;
+}
+
+void Lig4::reiniciarTabuleiro() {
+    for (int i = 0; i < this->linhas; i++) {
+        for (int j = 0; j < this->colunas; j++) {
             set_char(i, j, ' ');
+        }
+    }
 }
 
-void Lig4::mudarTurno()
-{
-    if (this->jogador_atual == 1)
-        this->jogador_atual = 2;
-    else if (this->jogador_atual == 2)
-        this->jogador_atual = 1;
+void Lig4::mudarTurno() {
+    if (this->jogador_atual == 1) this->jogador_atual = 2;
+    else if (this->jogador_atual == 2) this->jogador_atual = 1;
+}
+
+void Lig4::partida() {
+
 }
