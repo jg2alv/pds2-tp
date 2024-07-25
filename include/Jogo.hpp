49b06cedfcd53cf181367148ba16ce21dc4bcd27
HSThzz <thiagoesmerio123@gmail.com>
2024-07-25 15:04:00 -0300

    mudança na classe jogo

--- include/Jogo.hpp
+++ include/Jogo.hpp
@@ -26,7 +26,6 @@ public:
     virtual bool jogadaValida(const Jogada& jogada) const = 0;
     virtual bool verificarVitoria() const = 0;
     virtual bool verificarEmpate() const = 0;
-    virtual void reiniciarTabuleiro() = 0;
     virtual void partida()=0;
 };
 
