f567596bb45e5482785af3303e3aa0803e3c0ee3
João Vaz <119084806+jg2alv@users.noreply.github.com>
2024-08-04 14:36:07 -0300

    remocao das classes do xadrez

--- include/Xadrez.hpp
+++ include/Xadrez.hpp
@@ -9,52 +9,10 @@
 #include "Jogador.hpp"
 #include "Jogo.hpp"
 
-enum class Pecas {
-    PeaoBranco,
-    PeaoPreto,
-    TorreBranca,
-    TorrePreta,
-    CavaloBranco,
-    CavaloPreto,
-    BispoBranco,
-    BispoPreto,
-    ReiBranco,
-    ReiPreto,
-    RainhaBranca,
-    RainhaPreta
-};
-
-class Posicao {
-private:
-    int linha;
-    int coluna;
-
-public:
-    Posicao(int linha, int coluna);
-    int x();
-    int y();
-};
-
-class Peca {
-private:
-    Pecas tipo;
-    Posicao pos;
-
-public:
-    Peca(Pecas tipo, Posicao pos);
-    Pecas getTipo();
-    Posicao getPosicao();
-    char getChar();
-};
-
 class Xadrez : public Jogo {
-private:
-    std::map<std::string, std::vector<Peca*>> pecas;
-
 public:
     Xadrez(Jogador &jogador1, Jogador &jogador2);
     Xadrez(Jogador &jogador1, Jogador &jogador2, std::vector<std::vector<char>> tabuleiro);
-    void preencherTabuleiro();
     ~Xadrez();
 
     virtual std::string getNome() const override;
