8e21a90155b9ed44496ca76a13c542f73c93081e
MEduardaNunes <mecosta961@gmail.com>
2024-08-04 21:58:58 -0300

    documentacao da classe JogoDaVelha

--- include/JogoDaVelha.hpp
+++ include/JogoDaVelha.hpp
@@ -4,9 +4,27 @@
 #include <vector>
 #include <string>
 
+#include "Jogada.hpp"
 #include "Jogo.hpp"
 #include "Jogador.hpp"
 
+/**
+ * \class JogoDaVelha
+ * 
+ * \brief Classe que representa o jogo da velha
+ * 
+ * Jogo da velha é um jogo de tabuleiro (3 x 3) jogado por dois jogadores.
+ * O objetivo do jogo é que um jogador coloque no tabuleiro tres peças
+ * consecutivas de seu simbolo, seja em uma linha, coluna ou diagonal.
+ * O primeiro jogador que completar esse objetivo vence o jogo. Caso todo 
+ * o tabuleiro seja preenchido e nenhum jogador tenha conseguido colocar 
+ * tres peças consecutivas, os dois jogadores irao empatar e a partida 
+ * termina.
+ * Para jogar, o jogador da vez tera que digitar o numero de uma linha e de
+ * uma coluna do tabuleiro que deseja colocar sua peca. Caso as coordenadas
+ * nao estejam dentro dos limites do tabuleiro ou a posicao esteja ocupada, 
+ * o jogador devera digitar outra linha e outra coluna que sejam validas.
+ */
 class JogoDaVelha : public Jogo {
 private:
     bool tabuleiroCheio() const;
