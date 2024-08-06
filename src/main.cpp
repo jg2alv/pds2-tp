9fb4799a7e197a50739cf4e6d6c792378c3a6df7
João Vaz <119084806+jg2alv@users.noreply.github.com>
2024-08-06 10:51:23 -0300

    possivel commit final: FS penis implementado

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 #include <exception>
+#include <sstream>
 
 #include "Jogador.hpp"
 #include "Sistema.hpp"
 #include "Jogo.hpp"
+#include "Excecao.hpp"
 
 using namespace std;
 
@@ -15,39 +17,48 @@ int main() {
         while (!sistema.isSistemaFinalizado()) {
             cout << "Entre um comando ou digite ? para ajuda: ";
 
-            string candidato_a_comando;
-            cin >> candidato_a_comando;
+            string candidato_a_comando_string, candidato_a_comando;
+            getline(cin, candidato_a_comando_string);
+            stringstream candidato_a_comando_stream(candidato_a_comando_string);
+            candidato_a_comando_stream >> candidato_a_comando;
             try {
                 switch (Sistema::identificar_comando(candidato_a_comando)) {
                     case Sistema::Comando::CadastrarJogador: {
                         std::string apelido, nome;
-                        cin >> apelido;
-                        getline(cin, nome);
+                        candidato_a_comando_stream >> apelido;
+                        getline(candidato_a_comando_stream, nome);
                         sistema.cadastrarJogador(apelido, nome.erase(0, 1));
                         cout << "Jogador " << apelido << " cadastrado com sucesso\n";
                         break;
                     }
                     case Sistema::Comando::RemoverJogador: {
                         string apelido;
-                        cin >> apelido;
+                        candidato_a_comando_stream >> apelido;
+                        bool stream_terminou = candidato_a_comando_stream.eof();
+                        if(!stream_terminou) throw Excecao("RJ aceita apenas um argumento: RJ [apelido]");
                         sistema.removerJogador(apelido);
                         cout << "Jogador " << apelido << " removido com sucesso\n";
                         break;
                     }
                     case Sistema::Comando::ListarJogadores: {
-                        string criterio;
-                        cin >> criterio;
-                        sistema.listarJogadores(criterio, cout);
+                        string base;
+                        candidato_a_comando_stream >> base;
+                        bool stream_terminou = candidato_a_comando_stream.eof();
+                        if(!stream_terminou) throw Excecao("LJ aceita apenas um argumento: LJ [A | N]");
+                        sistema.listarJogadores(base, cout);
                         break;
                     }
                     case Sistema::Comando::ExecutarPartida: {
                         string nome_do_jogo, apelido1, apelido2;
-                        cin >> nome_do_jogo >> apelido1 >> apelido2;
-                        cin.ignore();
+                        candidato_a_comando_stream >> nome_do_jogo >> apelido1 >> apelido2;
+                        bool stream_terminou = candidato_a_comando_stream.eof();
+                        if(!stream_terminou) throw Excecao("EP aceita apenas 3 argumentos: EP [jogo] [apelido-1] [apelido-2]");
                         sistema.executarPartida(nome_do_jogo, apelido1, apelido2, cin, cout);
                         break;
                     }
                     case Sistema::Comando::FinalizarSistema: {
+                        bool stream_terminou = candidato_a_comando_stream.eof();
+                        if(!stream_terminou) throw Excecao("FS nao aceita argumentos");
                         sistema.finalizarSistema();
                         break;
                     }
