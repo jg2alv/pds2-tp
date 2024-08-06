6b1e03683634b87475c51c75a04a0ba3c0c620ac
João Vaz <119084806+jg2alv@users.noreply.github.com>
2024-08-05 22:13:28 -0300

    implementacao de 5 novas excecoes

--- include/Excecoes.hpp
+++ include/Excecoes.hpp
@@ -13,4 +13,49 @@ public:
     const char* what() const throw();
 };
 
+class ExcecaoDoJogoDaVelha : public Excecao {
+private: 
+    std::string _mensagem; 
+  
+public: 
+    ExcecaoDoJogoDaVelha(std::string mensagem);
+    const char* what() const throw();
+};
+
+class ExcecaoDoLig4 : public Excecao {
+private: 
+    std::string _mensagem; 
+  
+public: 
+    ExcecaoDoLig4(std::string mensagem);
+    const char* what() const throw();
+};
+
+class ExcecaoDoReversi : public Excecao {
+private: 
+    std::string _mensagem; 
+  
+public: 
+    ExcecaoDoReversi(std::string mensagem);
+    const char* what() const throw();
+};
+
+class ExcecaoDoSistema : public Excecao {
+private: 
+    std::string _mensagem; 
+  
+public: 
+    ExcecaoDoSistema(std::string mensagem);
+    const char* what() const throw();
+};
+
+class ExcecaoDoXadrez : public Excecao {
+private: 
+    std::string _mensagem; 
+  
+public: 
+    ExcecaoDoXadrez(std::string mensagem);
+    const char* what() const throw();
+};
+
 #endif
--- src/Excecoes.cpp
+++ src/Excecoes.cpp
@@ -9,3 +9,28 @@ Excecao::Excecao(string mensagem) : std::runtime_error(mensagem), _mensagem(mens
 const char* Excecao::what() const throw() {
     return _mensagem.c_str();
 }
+
+ExcecaoDoJogoDaVelha::ExcecaoDoJogoDaVelha(string mensagem) : Excecao(mensagem), _mensagem(mensagem) {};
+const char* ExcecaoDoJogoDaVelha::what() const throw() {
+    return _mensagem.c_str();
+}
+
+ExcecaoDoLig4::ExcecaoDoLig4(string mensagem) : Excecao(mensagem), _mensagem(mensagem) {};
+const char* ExcecaoDoLig4::what() const throw() {
+    return _mensagem.c_str();
+}
+
+ExcecaoDoReversi::ExcecaoDoReversi(string mensagem) : Excecao(mensagem), _mensagem(mensagem) {};
+const char* ExcecaoDoReversi::what() const throw() {
+    return _mensagem.c_str();
+}
+
+ExcecaoDoSistema::ExcecaoDoSistema(string mensagem) : Excecao(mensagem), _mensagem(mensagem) {};
+const char* ExcecaoDoSistema::what() const throw() {
+    return _mensagem.c_str();
+}
+
+ExcecaoDoXadrez::ExcecaoDoXadrez(string mensagem) : Excecao(mensagem), _mensagem(mensagem) {};
+const char* ExcecaoDoXadrez::what() const throw() {
+    return _mensagem.c_str();
+}
