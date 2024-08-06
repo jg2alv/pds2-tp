3ab3d5930ae30d66197060a7ef3d9ceb7ffc4a44
Spwner <henrique.lge175@gmail.com>
2024-08-06 20:28:29 -0300

    documentacao para Excecao

--- include/Excecao.hpp
+++ include/Excecao.hpp
@@ -5,6 +5,12 @@
 #include <exception>
 #include <iostream>
 
+
+/**
+ * \brief Classe de exceções genérica usada em todo o projeto.
+ *
+ * Essa é uma classe derivada de `std::runtime_error` que é lançada por diferentes partes do programa e capturada a fim de mostrar uma mensagem de erro.
+ */
 class Excecao : public std::runtime_error {
 private: 
     std::string _mensagem; 
--- src/Excecao.cpp
+++ src/Excecao.cpp
@@ -5,7 +5,23 @@
 
 using namespace std;
 
+/**
+ * \brief Esse costrutor recebe a mensagem assiciada a Excecao.
+ *
+ * A mensagem recebida por esse método é a mesma retornada por `Excecao::what()`.
+ * \see what()
+ * \param mensagem Mensagem associada à Excecao.
+ */
 Excecao::Excecao(string mensagem) : std::runtime_error(mensagem), _mensagem(mensagem) {};
+
+
+/**
+ * \brief Essa função retorna a mensagem associada à Excecao.
+ *
+ * O ponteiro retornado por essa função é para o caratere inicial uma C-style string igual à mensagem introduzida no construtor.
+ * \see Excecao()
+ * \return Mensagem associada à Excecao como uma C-style string.
+ */
 const char* Excecao::what() const throw() {
     return _mensagem.c_str();
 }
